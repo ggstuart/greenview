3dd1188e0e8ef67dc0d5db91e67d3ad56de31928
Graeme Stuart <ggstuart@gmail.com>
2011-08-10 23:22:10 +0100

    Added setup.py for distribution

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='greenview',
+    version='0.1.1',
+    description='Python Library for reading data from Greenview web service',
+    author='Graeme Stuart',
+    author_email='ggstuart@gmail.com',
+    py_modules=['greenview', 'update'],
+    url='https://github.com/ggstuart/greenview',
+)
