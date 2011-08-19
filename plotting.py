66c7d931fdcfad1f40337f971bcf4d9ee3426027
Graeme Stuart <ggstuart@gmail.com>
2011-08-19 15:37:01 +0100

    Upgraded plotting example

--- plotting.py
+++ plotting.py
@@ -1,11 +1,29 @@
 import greenview
 from matplotlib import pyplot as plt
-ws = greenview.WebService()
-meter_ids = [213, 69, 111, 15, 490]
+from matplotlib.dates import DateFormatter
+fmt = DateFormatter('%d %b')
 
-plt.figure()
-for meter_id in meter_ids:
+ws = greenview.WebService()
+
+labels = {
+    213: 'Queens',
+    69: 'IOCT',
+    111: 'Kimberlin Library',
+    15: 'Campus Centre',
+    490: 'Hugh Aston',
+}
+
+#meter_ids = [213, 69, 111, 15, 490]
+fig = plt.figure()
+fig.canvas.set_window_title("Greenview") 
+plt.suptitle('Latest Week')
+for meter_id in sorted(labels):# meter_ids:
+#    meter = ws.Meter(meter_id)
     latestWeek = ws.GraemeLatestWeek(meter_id)
     data = latestWeek.data()
-    plt.plot(data['datetime'], data['value'])
+    plt.plot(data['datetime'], data['value'], label=labels[meter_id])#meter.description)
+    plt.grid(True)
+leg = plt.legend(loc='best')
+leg.get_frame().set_alpha(0.15)
+plt.axes().xaxis.set_major_formatter(fmt)
 plt.show()
