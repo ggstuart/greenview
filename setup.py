c64d54777365e4f67e69d1193d3a3f5f23d9b1db
Graeme Stuart <ggstuart@gmail.com>
2011-09-08 12:21:33 +0100

    changed to setuptools for installation, removed examples from setup and added .gitignore

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
     name='greenview',
-    version='0.1.2',
+    version='0.1.3',
     description='Python Library for reading data from Greenview web service',
     author='Graeme Stuart',
     author_email='ggstuart@gmail.com',
-    packages = ['greenview', 'greenview.examples'],
+    packages = ['greenview'],
     package_dir = {'': 'lib'},
     url='https://github.com/ggstuart/greenview',
-    requires=['numpy'],
+    install_requires=['numpy'],
 )
